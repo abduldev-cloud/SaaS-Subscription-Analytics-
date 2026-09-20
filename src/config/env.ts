c904e87bab2225f731bb04c99101da62e6af52ad
Abdul <ah303130@gmail.com>
2026-09-20 14:38:25 +0530

    configure stripe sandbox

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,4 +4,5 @@ export const env = {
     port: Number(process.env.PORT) || 5000,
     databaseUrl: process.env.DATABASE_URL!,
     redisUrl: process.env.REDIS_URL!,    
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY!,
 };
